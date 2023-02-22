9dbfc6c3452cbf7dae0210f112843aa5234e91f4
Joseph D Rivera <josephdrivera15@gmail.com>
2023-02-22 15:27:28 -0500

    Add the API NPM  package

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 const route = require('./routes/users');
+const swaggerDocument = require('./swagger.json');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use(route);
 
